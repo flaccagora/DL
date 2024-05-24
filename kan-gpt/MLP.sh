cc711449ee399b0e0648ac6de2c09c66b82233d9
flaccagora <matteo.nunziante@gmail.com>
2024-05-24 13:32:41 +0200

    MLP

--- kan-gpt/MLP.sh
+++ kan-gpt/MLP.sh
@@ -10,7 +10,7 @@
 date
 
 cd ~/DL/kan-gpt
-
+conda deactivate
 source .NLP/bin/activate
 
 WANDB_MODE=online  python3 -m kan_gpt.train --architecture MLP --batch_size 32 --dataset webtext --max_iters 100 --num_gpus=4 --save 10000 --eval 10000
