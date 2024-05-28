381621786756c4f839fbd4dd38702cbbe1e6ed6d
flaccagora <matteo.nunziante@gmail.com>
2024-05-28 10:12:33 +0200

    train e kan argument

--- nanoGPT/train.py
+++ nanoGPT/train.py
@@ -156,6 +156,11 @@ if os.path.exists(meta_path):
 # model init
 model_args = dict(n_layer=n_layer, n_head=n_head, n_embd=n_embd, block_size=block_size,
                   bias=bias, vocab_size=None, dropout=dropout) # start with model_args from command line
+
+if architecture = 'Kan': 
+    model_args = dict(n_layer=n_layer, n_head=n_head, n_embd=n_embd, block_size=block_size,
+                  bias=bias, vocab_size=None, dropout=dropout, grid=grid, k=k) # start with model_args from command line
+
 if init_from == 'scratch':
     # init a new model from scratch
     print("Initializing a new model from scratch")
