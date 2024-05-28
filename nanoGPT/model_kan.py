525b35c4608724ca72482f35e472f2f01b46947c
flaccagora <matteo.nunziante@gmail.com>
2024-05-28 10:09:37 +0200

    train e kan argument

--- nanoGPT/model_kan.py
+++ nanoGPT/model_kan.py
@@ -116,6 +116,8 @@ class GPTConfig:
     n_embd: int = 768
     dropout: float = 0.0
     bias: bool = True # True: bias in Linears and LayerNorms, like GPT-2. False: a bit better and faster
+    grid: int = 3
+    k: int = 3
 
 class GPT(nn.Module):
 
